d66d8dac60208b2b59c1ef428ab6479d55f30a6e
Noir Joe <noirjoe@outlook.com>
2024-04-05 23:33:40 +1100

    TaskList now will display task with nearest dueDate on Top

--- src/components/UserMain.jsx
+++ src/components/UserMain.jsx
@@ -3,6 +3,13 @@ import SideBar from './Sidebar';
 import TaskList from './TaskList';
 
 function UserMain({ onSignoutClick, tasks, reviews }) {
+  // Sort to list the one with the nearest due date first
+  tasks.sort((a,b) => {
+    const aDate = new Date(a.dueDate);
+    const bDate = new Date(b.dueDate);
+    return aDate - bDate;
+  })
+
   return (
     <div className="grid grid-cols-5 h-full w-full">
       <div className="col-span-1">
